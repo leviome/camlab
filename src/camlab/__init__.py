13c27ab5e966ff061e31be75f37f8dc645563dcf
木盏 <muzhan@muzhandeMacBook-Pro.local>
2024-04-07 22:24:11 +0800

    Support 3DGS

--- src/camlab/__init__.py
+++ src/camlab/__init__.py
@@ -1,1 +1,19 @@
 from .camera import *
+
+def load_3dgs_camera(gs_cam):
+    """
+    Support 3D Gaussian Splatting Camera-Class Object
+    """
+    cam = CameraObj()
+    cam.manual_init(focal=fov2focal(cam_gs.FoVx, cam_gs.image_width),
+                    w=cam_gs.image_width, h=cam_gs.image_height)
+
+    ext = np.eye(4)
+    ext[:3, :3] = np.transpose(cam_gs.R)
+    ext[:3, 3] = cam_gs.T
+    c2w = np.linalg.inv(ext)
+    c2w[:3, 1:3] *= -1
+
+    cam.R = c2w[:3, :3]
+    cam.T = c2w[:3, 3]
+    return cam
